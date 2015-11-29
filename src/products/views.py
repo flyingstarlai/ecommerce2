9829da80a1bee4f4d14b837c131287101edc6d1c
flyingstarlai <flyingstarlai@gmail.com>
2015-11-29 20:00:28 +0800

    Inventory

--- src/products/views.py
+++ src/products/views.py
@@ -1,26 +1,77 @@
+from django.contrib import messages
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.db.models import Q
+from django.http import Http404
 # Create your views here.
 
+from .forms import VariationInventoryFormSet
+from .models import Product, Variation
 
-from .models import Product
 
+class VariationListView(ListView):
+    model = Variation
+    queryset = Variation.objects.all()
 
-class ProductDetailView(DetailView):
-    model = Product
-    # template_name = '<appname>/<modelname>_detail.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super(VariationListView, self).get_context_data(*args, **kwargs)
+        context['formset'] = VariationInventoryFormSet(queryset=self.get_queryset())
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        product_pk = self.kwargs.get("pk")
+        if product_pk:
+            product = get_object_or_404(Product, pk=product_pk)
+            queryset = Variation.objects.filter(product=product)
+        return queryset
+
+    def post(self, request, *args, **kwargs):
+        formset = VariationInventoryFormSet(request.POST, request.FILES)
+        print request.POST
+        if formset.is_valid():
+            formset.save(commit=False)
+            for form in formset:
+                new_item = form.save(commit=False)
+                if new_item.title:
+                    product_pk = self.kwargs.get("pk")
+                    product = get_object_or_404(Product, pk=product_pk)
+                    new_item.product = product
+                    new_item.save()
+
+            messages.success(request, "Your inventory and pricing has been updated")
+            return redirect("products")
+        raise Http404
 
 
 class ProductListView(ListView):
     model = Product
-    # queryset = Product.objects.filter(active=False)
+    queryset = Product.objects.all()
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
-        print context
+        context['now'] = timezone.now()
+        context['query'] = self.request.GET.get('q')  # None
         return context
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(ProductListView, self).get_queryset(*args, **kwargs)
+        query = self.request.GET.get("q")
+        if query:
+            qs = self.queryset.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
+            )
+            try:
+                qs2 = self.model.objects.filter(
+                         Q(price=query)
+                )
+                qs = (qs | qs2).distinct()
+            except:
+                pass
+        return qs
+
 
 # def product_detail_view_func(request, id):
 #     # product_instance = Product.objects.get(id=id)
@@ -31,3 +82,6 @@ class ProductListView(ListView):
 #     }
 #     return render(request, template, context)
 
+class ProductDetailView(DetailView):
+    model = Product
+    # template_name = '<appname>/<modelname>_detail.html'
